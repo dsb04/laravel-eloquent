0b4010c947debcc292a6614422fa0609cce486ac
Diogo Souza <bdiogo212@gmail.com>
2022-01-31 21:24:23 -0300

    f

--- app/Http/Requests/StoreBillRequest.php
+++ app/Http/Requests/StoreBillRequest.php
@@ -24,13 +24,13 @@ class StoreBillRequest extends FormRequest
     public function rules()
     {
         return [
-            'fature'=>'required',
+            'fature'=>'required'
         ];
     }
     public function messages()
     {
      return [
-     'fature.required' =>'Uma fatura é obrigatória',
+     'fature.required' =>'Uma fatura é obrigatória'
      ];
     }
 }
