309b468deebc50315060a681bbf117e38517d44e
Diogo Souza <bdiogo212@gmail.com>
2022-01-31 21:28:25 -0300

    bills

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,7 @@ Route::get('/clients/order',[
     ClientController::class, 
     'order'
 ]);
-Route::post('/bill/store', [
+Route::post('/bills/store', [
     BillController::class, 
     'store'
 ]);
