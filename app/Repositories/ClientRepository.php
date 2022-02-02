ff2e8e181395e33297bf91e2c1d693e948e8e317
Diogo <bdogo212@gmail.com>
2022-02-02 16:40:05 -0300

    aio

--- app/Repositories/ClientRepository.php
+++ app/Repositories/ClientRepository.php
@@ -4,7 +4,7 @@ namespace App\Repositories;
 
 use Prettus\Repository\Eloquent\BaseRepository;
 
-class PostRepository extends BaseRepository
+class ClientRepository extends BaseRepository
 {
     public function model()
 
