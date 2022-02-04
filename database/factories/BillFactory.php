2555b0c17402667146ea4da183d3bda01b40b98e
Diogo <bdogo212@gmail.com>
2022-02-03 22:36:29 -0300

    lalala

--- database/factories/BillFactory.php
+++ database/factories/BillFactory.php
@@ -17,8 +17,8 @@ class BillFactory extends Factory
             'invoice'=> $this->faker->number(4),
             'installment'=> $this->faker->randomDigit(1),
             'client_id'=> $this->faker->floatval(100),
-            'value'=> $this->faker->randomFloat($max=100),
-            'due_date'=> $this->faker->dateTimeBetween('now' , '+1 week'),
+            'value'=> $this->faker->randomFloat(0,0,100),
+            'due_date'=> $this->faker->dateTimeBetween('', '+1 week'),
             'payment_date'=> $this->faker->dateTimeBetween('-week')
         ];
     }
