8aa3541ae1c31b029680ec32fc68468fc983349b
Diogo Souza <bdiogo212@gmail.com>
2022-02-04 19:08:40 -0300

    ae

--- database/factories/BillFactory.php
+++ database/factories/BillFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Client;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class BillFactory extends Factory
@@ -14,7 +15,12 @@ class BillFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'invoice'=> $this->faker->randomNumber(4,true),
+            'installment'=>$this->faker->randomDigit(),
+            'client_id'=>Client::factory()->create()->id,
+            'value'=>$this->faker->randomFloat(0,0,100),
+            'due_date'=>$this->faker->dateTimeBetween('now','+1 weeks'),
+            'payment_date'=>$this->faker->dateTimeBetween('-1 week', 'now')
         ];
     }
 }
