795b5b0d8ff5c3912f1b94773f20d5efab72459f
Diogo <bdogo212@gmail.com>
2022-02-03 22:48:04 -0300

    hdyh

--- database/factories/BillFactory.php
+++ database/factories/BillFactory.php
@@ -16,7 +16,7 @@ class BillFactory extends Factory
         return [
             'invoice'=> $this->faker->randomnumber(4),
             'installment'=> $this->faker->randomDigit(),
-            'client_id'=> $this->faker->id(),
+            'client_id'=> User::factory()->create()->id,
             'value'=> $this->faker->randomFloat(100),
             'due_date'=> $this->faker->dateTimeInInterval('now', '+1 week'),
             'payment_date'=> $this->faker->dateTimeInInterval('-1 week', 'now')
